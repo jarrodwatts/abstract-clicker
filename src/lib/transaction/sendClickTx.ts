45148ccd44bcd3f1e8415a28d414a80bbd386321
Jarrod Watts <jarrod@cubelabs.xyz>
2025-05-22 10:19:03 +1000

    working with 4 calls

--- src/lib/transaction/sendClickTx.ts
+++ src/lib/transaction/sendClickTx.ts
@@ -12,7 +12,10 @@ import {
   parseAbiParameters,
   toFunctionSelector,
 } from "viem";
-import { SessionConfig } from "@abstract-foundation/agw-client/sessions";
+import {
+  createSessionClient,
+  SessionConfig,
+} from "@abstract-foundation/agw-client/sessions";
 import { walletClient } from "@/const/walletClient";
 import { getPeriodIdsForTransaction } from "../agw/getPeriodIdsForTransaction";
 import {
@@ -33,109 +36,27 @@ export default async function signClickTx(
   nonce: number,
   gas: bigint
 ) {
-  const transaction = {
+  const preparedTransaction = await walletClient.prepareTransactionRequest({
     to: COOKIE_CLICKER_CONTRACT_ADDRESS as `0x${string}`,
     data: toFunctionSelector("click()"),
+    type: "eip712",
     chain,
-    chainId: chain.id,
     nonce,
+    chainId: chain.id,
     maxPriorityFeePerGas: 0n,
     maxFeePerGas: 30000000n,
-    gas: 150000n,
-    from: agwAddress,
-    type: `eip712`, // EIP-712 transaction type for ZKsync
-    gasPerPubdata: 1212n, // Required for ZKsync transactions
-  };
+    gas: 350000n,
+  });
 
-  console.log(`transaction:`, transaction);
-
-  const validationHookData = {
-    [SESSION_KEY_VALIDATOR_ADDRESS]: encodeSessionWithPeriodIds(
-      session,
-      getPeriodIdsForTransaction({
-        sessionConfig: session,
-        target: transaction.to as `0x${string}`,
-        selector: (transaction.data?.slice(0, 10) ?? "0x") as Hex,
-        timestamp: BigInt(Math.floor(Date.now() / 1000)),
-      })
-    ),
-  };
-
-  console.log(`validationHookData:`, validationHookData);
-
-  transformHexValues(transaction, [
-    "value",
-    "nonce",
-    "maxFeePerGas",
-    "maxPriorityFeePerGas",
-    "gas",
-    "chainId",
-    "gasPerPubdata",
-  ]);
-
-  assertEip712Request({
+  const sessionClient = createSessionClient({
     account: agwAddress,
     chain,
-    ...(transaction as AssertEip712RequestParameters),
+    signer: sessionSigner,
+    session,
   });
 
-  if (!chain || VALID_CHAINS[chain.id] === undefined) {
-    throw new BaseError("Invalid chain specified");
-  }
-
-  if (!chain?.custom?.getEip712Domain)
-    throw new BaseError("`getEip712Domain` not found on chain.");
-
-  console.log(`transaction:`, transaction);
-
-  const transactionWithPaymaster = {
-    ...transaction,
-    from: agwAddress,
-    chainId: chain.id,
-  };
-
-  if (transactionWithPaymaster.data === undefined) {
-    // serializer turns undefined into 0x00 which causes issues sending
-    // eth to contracts that don't have a fallback function
-    transactionWithPaymaster.data = "0x";
-  }
-
-  console.log(`transactionWithPaymaster:`, transactionWithPaymaster);
-
-  const eip712Domain = chain?.custom.getEip712Domain({
-    ...transactionWithPaymaster,
-    type: "eip712",
-  });
-
-  console.log(`eip712Domain:`, eip712Domain);
-
-  const rawSignature = await signTypedData(walletClient, {
-    ...eip712Domain,
-    account: sessionSigner,
-  });
-
-  console.log(`rawSignature:`, rawSignature);
-
-  const hookData: Hex[] = [];
-
-  const validationHooks = await publicClient.readContract({
-    address: agwAddress,
-    abi: AGWAccountAbi,
-    functionName: "listHooks",
-    args: [true],
-  });
-
-  for (const hook of validationHooks) {
-    hookData.push(
-      validationHookData[hook as keyof typeof validationHookData] ?? "0x"
-    );
-  }
-
-  // Match the expect signature format of the AGW smart account
-  const signature = encodeAbiParameters(
-    parseAbiParameters(["bytes", "address", "bytes[]"]),
-    [rawSignature, SESSION_KEY_VALIDATOR_ADDRESS, hookData]
-  );
+  // @ts-ignore
+  const signature = await sessionClient.signTransaction(preparedTransaction);
 
   // 6. Send the raw transaction
   const response = await sendRawTransactionWithDetailedOutput(signature);
