1dc2579e243a7eb737858828c475954427122d12
Jarrod Watts <jarrod@cubelabs.xyz>
2025-05-27 15:34:52 +1000

    Fix build

--- src/hooks/useTotalClicks.ts
+++ src/hooks/useTotalClicks.ts
@@ -1,13 +1,12 @@
-import { useContractRead } from "wagmi";
+import { useReadContract } from "wagmi";
 import { COOKIE_CLICKER_CONTRACT_ABI as cookieClickerABI } from "@/const/contracts";
 import { cookieClickerAddress } from "@/const/addresses";
 
 export default function useTotalClicks() {
-  const { data: totalClicks, isLoading } = useContractRead({
+  const { data: totalClicks, isLoading } = useReadContract({
     address: cookieClickerAddress,
     abi: cookieClickerABI,
     functionName: "totalClicks",
-    watch: true,
   });
 
   return {
